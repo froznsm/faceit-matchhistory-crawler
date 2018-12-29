2d92a7fa32f384ed438ae3c3c4e29d42e1778def
Simon Keil <simon.keil@gmx.de>
2018-12-30 00:12:08 +0100

    now parsing all matchinfo and saving into dict

--- crawler.py
+++ crawler.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import datetime
 
 def simple_parser(url):
     code = requests.get(url)
@@ -22,9 +23,7 @@ def simple_parser(url):
 
 
 def js_parser(WebUrl):
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(options=options, executable_path='geckodriver.exe')
+    driver = webdriver.Firefox(executable_path='geckodriver.exe')
     driver.get(WebUrl)
     timeout = 10
     try:
@@ -35,20 +34,29 @@ def js_parser(WebUrl):
         driver.quit()
         exit()
     modal = driver.find_element_by_class_name("modal-content")
-    matches = driver.find_elements_by_class_name('match-history-stats__row')
-    print(len(matches))
+    match_elems = driver.find_elements_by_class_name('match-history-stats__row')
+    print(len(match_elems))
     for i in range(5):
-        driver.execute_script('arguments[0].scrollIntoView();', matches[-1])
+        driver.execute_script('arguments[0].scrollIntoView();', match_elems[-1])
         time.sleep(3)
-        matches += matches[-1].find_elements_by_xpath('following-sibling::tr')
-    print(len(matches))
-    for match in matches:
+        match_elems += match_elems[-1].find_elements_by_xpath('following-sibling::tr')
+    print(len(match_elems))
+    match_data = []
+    for match_elem in match_elems:
         try:
-            date = match.find_element_by_xpath(".//td[1]/span")
-            result = match.find_element_by_xpath(".//td[3]/div/span")
+            date_elem = match_elem.find_element_by_xpath(".//td[1]/span")
+            result_elem = match_elem.find_element_by_xpath(".//td[3]/div/span")
+            score_elem = match_elem.find_element_by_xpath(".//td[4]/div/span")
+            map_elem = match_elem.find_element_by_xpath(".//td[5]/div/span")
+            match_data.append({
+                'date' : datetime.datetime.strptime(date_elem.text+" 2018", "%d %b - %H:%M %Y"),
+                'result' : 1 if result_elem.text == "WIN" else 0,
+                'score' : score_elem.text,
+                'map' : map_elem.text
+            })
         except NoSuchElementException:
             continue
-        print('The match on {} was a {}'.format(date.text, result.text))
+    print(match_data)
     driver.quit()
 
 
