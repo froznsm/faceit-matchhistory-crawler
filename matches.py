21d8289b68bcf1bfbd4a2d3e076f6b3be83559aa
Simon Keil <simon.keil@gmx.de>
2019-01-01 16:16:37 +0100

    further reformatting

--- matches.py
+++ matches.py
@@ -11,26 +11,28 @@ matches = match_crawler.crawl_matches(datetime.datetime(2018, 11, 1), 10)
 def find_breaks_and_streaks(matches):
     matchtime = datetime.timedelta(hours = 7)
     streaks = []
-    streak = datetime.timedelta(0)
+    breaks = []
+    streak_duration = datetime.timedelta(0)
     streak_start = matches[0]['date']
     for match1, match2 in zip(matches[0:], matches[1:]):
         delta = match2['date'] - match1['date']
         if delta > matchtime:
             break_end = match2['date']
             streak_end = break_start = match1['date'] + datetime.timedelta(minutes = 90)
-            streak = streak_end - streak_start
-            game_break = break_end - break_start
+            streak_duration = streak_end - streak_start
+            break_duration = break_end - break_start
             streak_string = 'Streak from '+streak_start.strftime('%d %b - %H:%M')+' to '+streak_end.strftime('%d %b - %H:%M')
-            streak_duration_string = 'Streak lasted '+str(streak)
+            streak_duration_string = 'Streak lasted '+str(streak_duration)
             break_string = 'Break from '+break_start.strftime('%d %b - %H:%M')+' to '+break_end.strftime('%d %b - %H:%M')
-            break_duration_string = 'Break lasted '+str(game_break)
+            break_duration_string = 'Break lasted '+str(break_duration)
             print(streak_string, streak_duration_string)
             print(break_string, break_duration_string)
-            # print('Break from '+break_start.strftime('%d %b - %H:%M') +' until '+ match2['date'].strftime('%d %b - %H:%M'))
-            # print('It was '+str(delta - datetime.timedelta(minutes = 90))+' long.')
+            streak = (streak_start, streak_end, streak_duration)
             streaks.append(streak)
+            breake = (break_start, break_end, break_duration)
+            breaks.append(breake_duration)
             streak_start = match2['date']
-    print([str(s) for s in sorted(streaks, reverse=True)])
+    return (streaks, breaks)
 
 
 find_breaks_and_streaks(matches)
