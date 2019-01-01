8325e2c608b083149a005203e787dd60137c8f76
Simon Keil <simon.keil@gmx.de>
2019-01-01 18:43:41 +0100

    added comments and restructured code to make it more readable and reusable

--- crawler.py
+++ crawler.py
@@ -12,22 +12,33 @@ from wait import element_has_new_scroll_height
 
 class Crawler:
     """A crawler using selenium and bs4 to get the match history from a faceit csgo profile
-
-    url WebUrl of the profile
-    return a list of all matches up to a specified date_elem
+    The geckodriver.exe must be in the same path.
+    url -- A faceit profile URL (e.g. https://www.faceit.com/en/players-modal/PLAYERNAME/stats/csgo).
     """
     def __init__(self, url):
             self.url = url
 
+
     def get_url(self, *args):
         return self.url
 
+
     def crawl_matches(self, min_match_date, timeout=15, url = "", headless=True):
+        """Crawls a faceit profile URL (e.g. https://www.faceit.com/en/players-modal/PLAYERNAME/stats/csgo) for the match history.
+
+        min_match_date  --  Matches between now and this date will be returned.
+        timeout         --  Waiting time for each loading step until a TimeoutException is thrown and handled. (default 15)
+        url             --  The Profile URL (default "")
+        headless        --  Run with or without a browser window (default True)
+
+        return          --  A list of dictionaries, one per match. They contain the date, result, score and map for each match.
+        """
         if not url:
             url = self.url
         options = Options()
         if headless:
             options.headless = True
+
         driver = webdriver.Firefox(options=options, executable_path='geckodriver.exe')
         driver.get(url)
         try:
@@ -38,10 +49,13 @@ class Crawler:
             driver.quit()
             exit()
 
-
+        # Find the element of the whole Profile window
         modal = driver.find_element_by_class_name("modal-content")
+        # Find the last rendered match and its date
         last_elem = driver.find_elements_by_class_name('match-history-stats__row')[-1]
         last_match_date = datetime.datetime.strptime(last_elem.find_element_by_xpath(".//td[1]/span").text+" 2018", "%d %b - %H:%M %Y")
+
+        # scroll and load until a match older than the specified min_match_date is found
         while last_match_date > min_match_date:
             scrollHeight = driver.execute_script('return arguments[0].scrollHeight', modal)
             driver.execute_script('arguments[0].scrollIntoView(true);', last_elem)
@@ -53,20 +67,25 @@ class Crawler:
                 last_date = modal.find_elements_by_class_name('match-history-stats__row')[-1].find_element_by_xpath('.//td[1]/span').text
                 print("The oldest match loaded was played on "+ last_date)
                 break
+            # check if the last scroll and load brought in new matchess
             new_elems = last_elem.find_elements_by_xpath('following-sibling::tr')
             if new_elems:
                 last_elem = new_elems[-1]
             else:
                 print("No new matches found on last scroll")
+            # last match in all feasible cases in year 2018, too lazy solve this programmatically
             last_match_date = datetime.datetime.strptime(last_elem.find_element_by_xpath(".//td[1]/span").text+" 2018", "%d %b - %H:%M %Y")
 
+        # the page should be rendered now and we can now parse the code with bs4 for efficiency
         soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.quit()
         rows = soup.find_all('tr','match-history-stats__row')
         match_data = []
+        # go through the match history table (without the headrow) and append the data to the return list
         for match_elem in rows[1:]:
             cells = match_elem.find_all('td')
             date_elem = cells[0].find('span')
-            date = datetime.datetime.strptime(date_elem.get_text()+" 2018", "%d %b - %H:%M %Y")
+            date = datetime.datetime.strptime(date_elem.get_text()+" "+str(datetime.datetime.now().year), "%d %b - %H:%M %Y")
             if date < min_match_date:
                 break
             result_elem = cells[2].find('span')
@@ -78,5 +97,6 @@ class Crawler:
                 'score' : score_elem.get_text(),
                 'map' : map_elem.get_text()
             })
-        driver.quit()
+
+        # return the list in reversed order so that the oldest match comes first
         return match_data[::-1]
--- matches.py
+++ matches.py
@@ -1,38 +1,62 @@
 import datetime
 from crawler import Crawler
 
-player_name = 'nooky'
-
-match_crawler = Crawler('https://www.faceit.com/en/players-modal/{}/stats/csgo'.format(player_name))
-
-
-matches = match_crawler.crawl_matches(datetime.datetime(2018, 11, 1), 10)
 
 def find_breaks_and_streaks(matches):
-    matchtime = datetime.timedelta(hours = 7)
+    if not matches:
+        print("no matches")
+        return None
+    # average time a match will last (consider overtime on faceit)
+    average_matchtime = datetime.timedelta(minutes = 90)
+
+    # this duration and upwards is considered to break a streak of continuous playing
+    min_time_for_break = datetime.timedelta(hours = 5, minutes = 30)
+
     streaks = []
     breaks = []
-    streak_duration = datetime.timedelta(0)
     streak_start = matches[0]['date']
+
+    # compare each match with the next and check whether the time in between breaks the streak
     for match1, match2 in zip(matches[0:], matches[1:]):
-        delta = match2['date'] - match1['date']
-        if delta > matchtime:
+        delta = match2['date'] - (match1['date'] + average_matchtime)
+        if delta > min_time_for_break:
+            # assign streak and break ends and beginnings
             break_end = match2['date']
-            streak_end = break_start = match1['date'] + datetime.timedelta(minutes = 90)
+            streak_end = break_start = match1['date'] + average_matchtime
             streak_duration = streak_end - streak_start
             break_duration = break_end - break_start
+
+            # create output strings and print them
             streak_string = 'Streak from '+streak_start.strftime('%d %b - %H:%M')+' to '+streak_end.strftime('%d %b - %H:%M')
             streak_duration_string = 'Streak lasted '+str(streak_duration)
             break_string = 'Break from '+break_start.strftime('%d %b - %H:%M')+' to '+break_end.strftime('%d %b - %H:%M')
             break_duration_string = 'Break lasted '+str(break_duration)
             print(streak_string, streak_duration_string)
             print(break_string, break_duration_string)
+
+            # append streaks and breaks to corresponding lists
             streak = (streak_start, streak_end, streak_duration)
             streaks.append(streak)
             breake = (break_start, break_end, break_duration)
-            breaks.append(breake_duration)
+            breaks.append(break_duration)
+
+            # reset the startdate of the next streak
             streak_start = match2['date']
+
+    longest_streak = max(streaks, key=lambda x: x[2])[2]
+    print('The longest continuous time playing was: '+str(longest_streak))
     return (streaks, breaks)
 
+
+# name of the profile to crawl
+player_name = 'eXo'
+# date from which to now matches will be in the list
+min_date = datetime.datetime(2018, 11, 1)
+
+# initalise crawler with url
+match_crawler = Crawler('https://www.faceit.com/en/players-modal/{}/stats/csgo'.format(player_name))
+
+# call the crawler with the date
+matches = match_crawler.crawl_matches(min_date, 10)
 
 find_breaks_and_streaks(matches)
